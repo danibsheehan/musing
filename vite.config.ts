81f7d2215582ba264265795721cfe273fe54ac9e
Danielle Sheehan <danibsheehan@gmail.com>
2026-04-14 20:36:36 -0400

    update to run coverage and summary

--- vite.config.ts
+++ vite.config.ts
@@ -69,7 +69,7 @@ export default defineConfig({
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
     coverage: {
       provider: "v8",
-      reporter: ["text", "text-summary", "html", "lcov"],
+      reporter: ["text", "text-summary", "html", "lcov", "cobertura"],
       reportsDirectory: "./coverage",
       exclude: ["src/test/**"],
       /**
